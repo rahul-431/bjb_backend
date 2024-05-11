b4098fa6660d0ad3e05f13f955b16c66d7cb9b50
Rahul Mijar <rahul.mijar@trunary.com>
2024-05-11 15:25:42 +0545

    feat: getting all users and delete user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { uploadLocally } from "../middlewares/fileUpload.middleware.js";
-import { login, registerUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  getAllUsers,
+  login,
+  registerUser,
+} from "../controllers/user.controller.js";
 const userRouter = Router();
+
+//add new user
 userRouter.route("/").post(
   uploadLocally.fields([
     {
@@ -11,5 +18,13 @@ userRouter.route("/").post(
   ]),
   registerUser
 );
+
+//login user
 userRouter.route("/login").post(login);
+
+//get list of all users
+userRouter.route("/").get(getAllUsers);
+
+//delete user
+userRouter.route("/:id").delete(deleteUser);
 export default userRouter;
